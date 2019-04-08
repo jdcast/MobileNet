1c88c69fa2013990c0958430db98704a8913e470
jdcast <john.d.cast@gmail.com>
2019-04-07 22:52:40 -0700

    Attempt at q3, part 3 for inference for polarr-take-home-project...complains about missing variables when loading the model from checkpoint file and running the eval_image_classifier.py for inference on single polarr image

--- datasets/dataset_factory.py
+++ datasets/dataset_factory.py
@@ -23,6 +23,7 @@ from datasets import flowers
 from datasets import imagenet
 from datasets import mnist
 from datasets import kitti
+from datasets import polarr
 
 datasets_map = {
     'cifar10': cifar10,
@@ -30,6 +31,7 @@ datasets_map = {
     'imagenet': imagenet,
     'mnist': mnist,
     'kitti': kitti,
+    'polarr': polarr,
 }
 
 
